8e78045f1d01a8e1aa22d54481bc9a4eecad33f3
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-01 16:53:47 +0630

    Create home_notifier.dart

---
+++ lib/home_notifier.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final homeNotifierProvider = StateProvider<int>((ref) {
+  return 0;
+});
+
+final selectedGrainNotifierProvider = StateProvider<int>((ref) {
+  return 0;
+});
