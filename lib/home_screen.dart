a15a79f3a9bc63b397509b4f6b1a39878d55ebfe
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-01 16:54:00 +0630

    Create home_screen.dart

---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mcx/chart/chart_screen.dart';
+import 'package:mcx/home_notifier.dart';
+import 'package:mcx/market/market_screen.dart';
+import 'package:mcx/profile/profile_screen.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  final screens = [MarketScreen(), ChartScreen(), ProfileScreen()];
+  @override
+  Widget build(BuildContext context) {
+    final currentIndex = ref.watch(homeNotifierProvider);
+    return Scaffold(
+      body: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+
+        child: KeyedSubtree(
+          key: ValueKey<int>(currentIndex),
+          child: screens[currentIndex],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          ref.read(homeNotifierProvider.notifier).state = value;
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_rounded),
+            label: 'Market',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_rounded),
+            label: 'Chart',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
