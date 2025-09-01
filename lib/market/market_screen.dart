eb398fe8e35438779e9f01308de7e278a798aa55
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-01 16:54:03 +0630

    Create market_screen.dart

---
+++ lib/market/market_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mcx/data/market_notifier.dart';
+import 'package:mcx/home_notifier.dart';
+
+class MarketScreen extends ConsumerStatefulWidget {
+  const MarketScreen({super.key});
+
+  @override
+  ConsumerState<MarketScreen> createState() => _MarketScreenState();
+}
+
+class _MarketScreenState extends ConsumerState<MarketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final market = ref.watch(marketProvider);
+    ref.invalidate(selectedGrainNotifierProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('MCX . Market')),
+      body: ListView.separated(
+        itemCount: market.length,
+        separatorBuilder: (context, index) => Divider(),
+        itemBuilder: (context, index) {
+          final grain = market[index];
+          return ListTile(
+            title: Text(grain.name),
+            onTap: () {
+              ref.read(selectedGrainNotifierProvider.notifier).state = index;
+              ref.read(homeNotifierProvider.notifier).state = 1;
+            },
+            trailing: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: '${grain.openPrice} . ',
+                    style: const TextStyle(color: Colors.black, fontSize: 14),
+                  ),
+                  TextSpan(
+                    text: '${grain.currentPrice}',
+                    style: TextStyle(
+                      color: grain.currentPrice >= grain.openPrice
+                          ? Colors.green
+                          : Colors.red,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
