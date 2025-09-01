f82937787c0d1c5054ba2a91449064ed3bdfa672
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-01 16:53:44 +0630

    Create market_notifier.dart

---
+++ lib/data/market_notifier.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Grain {
+  final String name;
+  final int openPrice;
+  final int currentPrice;
+
+  Grain({
+    required this.name,
+    required this.openPrice,
+    required this.currentPrice,
+  });
+
+  Grain copyWith({int? currentPrice}) {
+    return Grain(
+      name: name,
+      openPrice: openPrice,
+      currentPrice: currentPrice ?? this.currentPrice,
+    );
+  }
+}
+
+class MarketNotifier extends StateNotifier<List<Grain>> {
+  MarketNotifier()
+    : super([
+        Grain(name: 'Rice', openPrice: 100000, currentPrice: 100000),
+        Grain(name: 'Bean', openPrice: 150000, currentPrice: 150000),
+        Grain(name: 'Chilli', openPrice: 50000, currentPrice: 50000),
+      ]) {
+    _startSimulation();
+  }
+
+  final _random = Random();
+  Timer? _timer;
+
+  void _startSimulation() {
+    _timer = Timer.periodic(const Duration(seconds: 3), (_) {
+      _updatePrices();
+    });
+  }
+
+  void _updatePrices() {
+    state = state.map((grain) {
+      final changeOptions = [500, -500, 1000, -1000];
+      final change = changeOptions[_random.nextInt(changeOptions.length)];
+      return grain.copyWith(currentPrice: grain.currentPrice + change);
+    }).toList();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+}
+
+final marketProvider = StateNotifierProvider<MarketNotifier, List<Grain>>((
+  ref,
+) {
+  return MarketNotifier();
+});
