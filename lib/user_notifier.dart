5894efdc6ca17d70a53453f58746159b295c6ab4
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-02 16:59:37 +0630

    Update user_notifier.dart

--- lib/user_notifier.dart
+++ lib/user_notifier.dart
@@ -1,14 +1,30 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mcx/data/market_notifier.dart';
+
 const maximumBalance = 1000000;
-final userNotifierProvider = NotifierProvider<UserNotifier, UserState>(UserNotifier.new);
+final userNotifierProvider = NotifierProvider<UserNotifier, UserState>(
+  UserNotifier.new,
+);
+
 class UserState {
   final int currentBalance;
-
-  UserState({ this.currentBalance = maximumBalance});
+  final UserPossession possession;
+  UserState({
+    this.currentBalance = maximumBalance,
+    this.possession = const UserPossession(grain: 'RICE', count: 10),
+  });
 }
+
 class UserNotifier extends Notifier<UserState> {
   @override
   UserState build() {
     return UserState();
   }
+}
+
+class UserPossession {
+  final String grain;
+  final double count;
+
+  const UserPossession({required this.grain, required this.count});
 }
