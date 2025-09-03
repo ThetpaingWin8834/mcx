615e293d69110df5c9da45ef865f1f122d0f088d
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-03 10:54:15 +0630

    Update market_screen.dart

--- lib/market/market_screen.dart
+++ lib/market/market_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:mcx/data/market_notifier.dart';
 import 'package:mcx/home_notifier.dart';
 
@@ -30,31 +31,99 @@ class _MarketScreenState extends ConsumerState<MarketScreen> {
               ),
             ),
             child: ListTile(
-              title: Text(grain.name),
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.grass,
+                        size: 18,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      const SizedBox(width: 6),
+                      Text(
+                        grain.name,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          // letterSpacing: 0.5,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'per TON',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontStyle: FontStyle.italic,
+                      color: Theme.of(
+                        context,
+                      ).colorScheme.onSurface.withOpacity(0.6),
+                    ),
+                  ),
+                ],
+              ),
               onTap: () {
                 ref.read(selectedGrainNotifierProvider.notifier).state = index;
                 ref.read(homeNotifierProvider.notifier).state = 1;
               },
-              trailing: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: '${grain.openPrice} . ',
-                      style: const TextStyle(color: Colors.black, fontSize: 14),
+              trailing: Column(
+                spacing: 2,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '${formatCurrencyAmount(grain.openPrice)} . ',
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '${formatCurrencyAmount(grain.currentPrice)} ',
+                          style: TextStyle(
+                            color: grain.currentPrice >= grain.openPrice
+                                ? Colors.green
+                                : Colors.red,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
+                  ),
+                  Text.rich(
                     TextSpan(
-                      text: '${grain.currentPrice}',
-                      style: TextStyle(
-                        color: grain.currentPrice >= grain.openPrice
-                            ? Colors.green
-                            : Colors.red,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      children: [
+                        WidgetSpan(
+                          child: Icon(
+                            grain.currentPrice >= grain.openPrice
+                                ? Icons.trending_up_rounded
+                                : Icons.trending_down_rounded,
+                            size: 16,
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              '  ${(((grain.currentPrice - grain.openPrice) / grain.openPrice) * 100).toStringAsFixed(1)}%',
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                    style: TextStyle(
+                      color: grain.currentPrice >= grain.openPrice
+                          ? Colors.green
+                          : Colors.red,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
               ),
             ),
           );
@@ -62,4 +131,9 @@ class _MarketScreenState extends ConsumerState<MarketScreen> {
       ),
     );
   }
+}
+
+String formatCurrencyAmount(num number) {
+  final numberFormat = NumberFormat('###,###,###,###,###,###.##');
+  return numberFormat.format(number);
 }
