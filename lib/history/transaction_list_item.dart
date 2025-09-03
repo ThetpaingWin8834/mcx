1eb20c4506b9f37da7eaa8aae964f84624e55607
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-03 10:00:12 +0630

    Create transaction_list_item.dart

---
+++ lib/history/transaction_list_item.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:mcx/history/transaction_item.dart';
+
+class TransactionListItem extends StatelessWidget {
+  final TransactionItem item;
+
+  const TransactionListItem({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Color typeColor = item.type.contains('sell, out')
+        ? Colors.red
+        : Colors.blue;
+    final Color profitColor = (item.profit ?? 0) >= 0
+        ? Colors.blue
+        : Colors.red;
+
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Theme.of(context).colorScheme.surfaceContainerHighest,
+          ),
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "${item.pair},",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      '${item.type}',
+                      style: TextStyle(color: typeColor, fontSize: 14),
+                    ),
+                  ],
+                ),
+                Text(
+                  'in ${item.lotSize} at ${item.price}',
+                  style: TextStyle(color: Colors.grey.shade700, fontSize: 12),
+                ),
+              ],
+            ),
+          ),
+          if (item.profit != null)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  item.timestamp,
+                  style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                ),
+                Text(
+                  item.profit!.toStringAsFixed(2),
+                  style: TextStyle(
+                    color: profitColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            )
+          else
+            Text(
+              item.timestamp,
+              style: TextStyle(color: Colors.grey[600], fontSize: 12),
+            ),
+        ],
+      ),
+    );
+  }
+}
