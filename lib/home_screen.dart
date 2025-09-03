7d6f389bd736d9a99e5d0233b2c596d743521c46
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-03 10:36:17 +0630

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:mcx/chart/chart_screen.dart';
 import 'package:mcx/history/transaction_screen.dart';
 import 'package:mcx/home_notifier.dart';
 import 'package:mcx/market/market_screen.dart';
+import 'package:mcx/navigation_colors_wrapper.dart';
 import 'package:mcx/profile/profile_screen.dart';
 import 'package:mcx/trade/trade_screen.dart';
 
@@ -25,40 +26,42 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final currentIndex = ref.watch(homeNotifierProvider);
-    return Scaffold(
-      body: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 300),
+    return NavigationColorsWrapper(
+      child: Scaffold(
+        body: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
 
-        child: KeyedSubtree(
-          key: ValueKey<int>(currentIndex),
-          child: screens[currentIndex],
+          child: KeyedSubtree(
+            key: ValueKey<int>(currentIndex),
+            child: screens[currentIndex],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: currentIndex,
-        onTap: (value) {
-          ref.read(homeNotifierProvider.notifier).state = value;
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart_rounded),
-            label: 'Market',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart_rounded),
-            label: 'Chart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.multiple_stop),
-            label: 'Trade',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history_rounded),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: currentIndex,
+          onTap: (value) {
+            ref.read(homeNotifierProvider.notifier).state = value;
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_rounded),
+              label: 'Market',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart_rounded),
+              label: 'Chart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.multiple_stop),
+              label: 'Trade',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history_rounded),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
       ),
     );
   }
